e9f279d9c9e82eca0c2275b0ff66ea672e1d7a38
forbzie <forbzie22@gmail.com>
2018-05-10 16:29:42 +0100

    adding install-baseapps PS script

---
+++ Install-BaseApplications.ps1
@@ -0,0 +1,26 @@
+﻿$VerbosePreference = 'continue'
+
+if (get-command choco.exe -ErrorAction SilentlyContinue)
+{
+    $chocoVersion = choco --version
+    Write-Verbose ("Chcolately version {0} Installed"-f $chocoVersion)
+}
+else
+{
+    Write-Verbose "Installing Chocolatey"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))  
+}
+
+try
+{
+    choco install Notepadplusplus --x86 -y
+    choco install virtualbox -y
+    choco install vagrant -y
+    choco install googlechrome -y
+    choco install git -y
+    choco install github -y
+}
+catch
+{
+    write-verbose $_
+}
